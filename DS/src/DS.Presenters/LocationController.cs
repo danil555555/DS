de09eefdd250f3db195202e9072d3673003803ed
Den <maslovdanil45228@gmail.com>
2026-03-02 18:53:31 +0300

    Create location implementation

---
+++ DS/src/DS.Presenters/LocationController.cs
@@ -0,0 +1,19 @@
+﻿using DS.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DS.Presenters;
+
+
+[ApiController]
+[Route("api/locations")]
+
+public class LocationController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        [FromServices] CreateLocationHandler handler,
+        [FromBody] CreateLocationRequest request, CancellationToken cancellationToken)
+    {
+        var rslt = await 
+    }
+}
